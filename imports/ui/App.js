c67746e96d32bdce312fa46e623ff6e9229b4c61
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-17 15:20:34 +0100

    Hide completed tasks and display incomplete tasks number

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -9,9 +9,26 @@ import Task from './Task.js';
 
 // App component - represents the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hideCompleted: false
+        }
+    }
+
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted
+        });
+    }
 
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -35,7 +52,17 @@ class App extends Component {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
+                    <label className='hide-completed'>
+                        <input
+                            type='checkbox'
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Tasks
+                    </label>
+
                     <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
                         <input
                             type="text"
@@ -55,6 +82,7 @@ class App extends Component {
 
 export default withTracker(() => {
     return {
-        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true} }).count()
     };
 })(App);
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
+import { Tasks } from '../api/tasks';
+
 export default class Task extends Component {
+
+    deleteThisTask() {
+        Tasks.remove(this.props.task._id);
+    }
+
+    toggleChecked() {
+        Tasks.update(this.props.task._id, {
+            $set: { checked: !this.props.task.checked }
+        });
+    }
+
+
     render() {
+        const taskClassName = this.props.task.checked ? 'checked' : '';
+
         return(
-            <li>{this.props.task.text}</li>
+            <li className={taskClassName}>
+                <button className='delete' onClick={this.deleteThisTask.bind(this)}>
+                    &times;
+                </button>
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={!!this.props.task.checked}
+                    onClick={this.toggleChecked.bind(this)}
+                />
+
+                <span className='text'>{this.props.task.text}</span>
+            </li>
         )
     }
 }
